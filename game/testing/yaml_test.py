51224e0809f628baef28773693fa4913da4a75f5
veggiebob <veggiebobowman@gmail.com>
2020-01-29 13:02:31 -0700

    changed test

--- game/testing/yaml_test.py
+++ game/testing/yaml_test.py
@@ -1,4 +1,4 @@
 import yaml
 towers_config = yaml.load(open("../../config/entities/towers.yaml").read(), Loader=yaml.FullLoader)
 print(yaml.dump(towers_config))
-print('basic tower range: %d'%towers_config['advanced_tower']['range'])
+print('basic tower range: %d'%towers_config['advanced_tower']['projectile'])
