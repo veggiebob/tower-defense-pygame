5106820560d414295f99a0e53822cd65ea9eb4f1
Sophia <20sophia.duncan@boiseschools.net>
2020-02-07 12:51:05 -0700

    Got the file parsing working

--- game/subsystems/environment.py
+++ game/subsystems/environment.py
@@ -11,15 +11,14 @@ class Environment():
 
     #Opens the map and puts it as the board
     def readFile(self, whatLevel):
+        global board
+        board = []
         level = []
         if whatLevel == 0:
-            levelOne = open('config/maps/testMap.txt')
+            levelOne = open('../../config/maps/testMap.txt').read()
 
             for line in levelOne:
                 level.append(line.rstrip().split(' '))
-
-        for i in range(0, len(level), 1):
-            print(level[i])
 
         for i in range(0, len(level[0]), 1):
             board.append([])
@@ -40,24 +39,38 @@ class Environment():
                 if array[i][j] == "E":
                     board[j][i].changeEnd(True)
 
+    #Places the tower if it can be placed there
+    def placeTower(self, posX, posY):
+        if self.adjacentPath(posX, posY):
+            board[posX][posY].changeTower(True)
+
     #Determines if a tower can be placed in the specified position
-    def towerPlacement(self, posX, posY):
-        randInt = 2
+    def adjacentPath(self, posX, posY):
+        if board[posX][posY - 1].getPath() or board[posX][posY - 1].getEnd():
+            return True
+        elif board[posX][posY + 1].getPath() or board[posX][posY + 1].getEnd():
+            return True
+        elif board[posX - 1][posY].getPath() or board[posX - 1][posY].getEnd():
+            return True
+        elif board[posX + 1][posY].getPath() or board[posX + 1][posY].getEnd():
+            return True
+
+        return False
 
     #Determines the enemy's next step, and takes in the enemy's current x and y
     def futurePath(self, posX, posY):
         tempX, tempY = posX, posY
 
-        endFound = False
-        while not endFound:
-            if board[tempX][tempY - 1].getPath():
-                tempY = tempY - 1
-            elif board[tempX][tempY + 1].getPath():
-                tempY = tempY + 1
-            elif board[tempX - 1][tempY].getPath():
-                tempX = tempX - 1
-            elif board[tempX + 1][tempY].getPath():
-                tempX = tempX + 1
+        if board[tempX][tempY - 1].getPath() or board[tempX][tempY - 1].getEnd():
+            tempY = tempY - 1
+        elif board[tempX][tempY + 1].getPath() or board[tempX][tempY + 1].getEnd():
+            tempY = tempY + 1
+        elif board[tempX - 1][tempY].getPath() or board[tempX - 1][tempY].getEnd():
+            tempX = tempX - 1
+        elif board[tempX + 1][tempY].getPath() or board[tempX + 1][tempY].getEnd():
+            tempX = tempX + 1
+
+        #return a point
 
 
 # What is contained in a single grid point
@@ -72,6 +85,15 @@ class SingleGrid():
 
     def getPath(self):
         return self.hasPath
+
+    def getEnemy(self):
+        return self.hasEnemy
+
+    def getTower(self):
+        return self.hasTower
+
+    def changeTower(self, newTower):
+        self.hasTower = newTower
 
     def changePath(self, newPath):
         self.hasPath = newPath
