667a4413cb9b37d89375fc7966ccb005be5ee98a
Sophia <20sophia.duncan@boiseschools.net>
2020-02-12 12:59:55 -0700

    Working on the futurePoint method

--- game/subsystems/environment.py
+++ game/subsystems/environment.py
@@ -2,12 +2,13 @@
 # _boardY --> The y dimensions of the grid
 class Environment():
     import pygame, sys
+    from game.common.math import Point
 
     def __init__(self):
         self.board = []
         self.readFile(0)
 
-    #Opens the map and puts it as the board
+    # Opens the map and puts it as the board
     def readFile(self, whatLevel):
         level = []
         if whatLevel == 0:
@@ -16,30 +17,30 @@ class Environment():
             for line in levelOne:
                 level.append(line.rstrip().split(' '))
 
-        for i in range(0, len(level), 1):
+        for i in range(0, len(level[0]), 1):
             self.board.append([])
             for j in range(0, len(level), 1):
                 self.board[i].append(SingleGrid())
 
         self.createBoard(level)
 
-    #Puts the map into the board array
+    # Puts the map into the board array
     def createBoard(self, array):
         for i in range(0, len(array), 1):
             for j in range(0, len(array[i]), 1):
-                #P = Path
+                # P = Path
                 if array[i][j] == "P":
                     self.board[j][i].changePath(True)
-                #E = End
+                # E = End
                 if array[i][j] == "E":
                     self.board[j][i].changeEnd(True)
 
-    #Places the tower if it can be placed there
+    # Places the tower if it can be placed there
     def placeTower(self, posX, posY):
         if self.adjacentPath(posX, posY):
             self.board[posX][posY].changeTower(True)
 
-    #Determines if a tower can be placed in the specified position
+    # Determines if a tower can be placed in the specified position
     def adjacentPath(self, posX, posY):
         if self.board[posX][posY - 1].getPath() or self.board[posX][posY - 1].getEnd():
             return True
@@ -52,7 +53,7 @@ class Environment():
 
         return False
 
-    #Determines the enemy's next step, and takes in the enemy's current x and y
+    # Determines the enemy's next step, and takes in the enemy's current x and y
     def futurePath(self, posX, posY):
         tempX, tempY = posX, posY
 
@@ -65,16 +66,16 @@ class Environment():
         elif self.board[tempX + 1][tempY].getPath() or self.board[tempX + 1][tempY].getEnd():
             tempX = tempX + 1
 
-        #return a point
+        return self.Point(tempX, tempY)
 
 # What is contained in a single grid point
 class SingleGrid():
     REQ_ATTRS = ["hasTower", "hasEnemy", "hasPath", "hasEnd"]
     DEFAULT_ATTRIBUTES = {
-        "hasTower" : False,
-        "hasEnemy" : False,
-        "hasPath" : False,
-        "hasEnd" : False
+        "hasTower": False,
+        "hasEnemy": False,
+        "hasPath": False,
+        "hasEnd": False
     }
 
     def getPath(self):
