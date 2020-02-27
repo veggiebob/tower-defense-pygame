8cde16b4ad8d82da5d0907a3e33c89746ac25704
Sophia <20sophia.duncan@boiseschools.net>
2020-02-26 20:45:53 -0700

    Working on creating multiple pathes

--- game/subsystems/environment.py
+++ game/subsystems/environment.py
@@ -4,7 +4,7 @@ class Environment():
 
     def __init__(self):
         self.board = []
-        self.path = []
+        self.pathes = []
         self.readFile(0)
         self.createPath()
 
@@ -32,6 +32,8 @@ class Environment():
                     self.board[j][i].changePath(True)
                 if array[i][j] == "E":
                     self.board[j][i].changeEnd(True)
+                if array[i][j] == "S":
+                    self.board[j][i].changeStart(True)
 
     # Places the tower if it can be placed there
     def placeTower(self, posX, posY):
@@ -64,31 +66,98 @@ class Environment():
             canGo.append(self.Point(tempX + 1, tempY))
         return canGo[self.random.randrange(0, len(canGo), 1)]
 
+    def returnPlaces(self, posX, posY, pastX, pastY):
+        tempX, tempY, canGo = posX, posY, []
+
+        if tempY - 1 != pastY and (self.board[tempX][tempY - 1].getPath() or self.board[tempX][tempY - 1].getEnd()):
+            canGo.append(self.Point(tempX, tempY - 1))
+        if tempY + 1 != pastY and (self.board[tempX][tempY + 1].getPath() or self.board[tempX][tempY + 1].getEnd()):
+            canGo.append(self.Point(tempX, tempY + 1))
+        if tempX - 1 != pastX and (self.board[tempX - 1][tempY].getPath() or self.board[tempX - 1][tempY].getEnd()):
+            canGo.append(self.Point(tempX - 1, tempY))
+        if tempX + 1 != pastX and (self.board[tempX + 1][tempY].getPath() or self.board[tempX + 1][tempY].getEnd()):
+            canGo.append(self.Point(tempX + 1, tempY))
+        return canGo
+
     #Creates the a list with all the path points
     def createPath(self):
+        path, numPathes = [], 0
         for i in range(0, len(self.board), 1):
             for j in range(0, len(self.board[i]), 1):
-                if self.board[i][j].getPath():
-                    self.path.append(self.Point(i, j))
+                if self.board[i][j].getStart():
+                    path.append(self.Point(i, j))
+
+        lastX, lastY, nowX, nowY, temp, endsFound = path[0].getX(), path[0].getY(), 0, 0, [], 0
+        self.pathes.append(path)
+
+        if lastX != 0:
+            if self.board[lastX - 1][lastY].getPath():
+                self.pathes[0].addPoint(self.Point(lastX - 1, lastY))
+        elif lastX != len(self.board):
+            if self.board[lastX + 1][lastY].getPath():
+                self.pathes[0].addPoint(self.Point(lastX + 1, lastY))
+        elif lastY != 0:
+            if self.board[lastX][lastY - 1].getPath():
+                self.pathes[0].addPoint(self.Point(lastX, lastY - 1))
+        elif lastY != len(self.board[lastX]):
+            if self.board[lastX][lastY + 1].getPath():
+                self.pathes[0].addPoint(self.Point(lastX, lastY + 1))
+
+        while endsFound != len(self.pathes):
+            for i in range(0, len(self.pathes), 1):
+                currentPathX = self.pathes[i].returnPoint(len(self.pathes[i].returnLength()) - 1).getX()
+                currentPathY = self.pathes[i].returnPoint(len(self.pathes[i].returnLength()) - 1).getY()
+
+                temp = self.returnPlaces(nowX, nowY, currentPathX, currentPathY)
+                if len(temp) > 1:
+                    for j in range(0, len(temp), 1):
+                        self.pathes.append(self.pathes[i])
+                        self.pathes[i + j].addPoint(temp[j])
+                else:
+                    self.pathes[i].addPoint(temp[0])
+
+
+
+            endsFound = 0
+            for i in range(0, len(self.pathes), 1):
+                tempPoint = self.pathes[i].returnPoint(len(self.pathes[i]))
+                if self.board[tempPoint.getX()][tempPoint.getY()].getEnd() :
+                    endsFound += 1
 
     #Given a time, returns the position an enemy should be at
-    def timeToPos(self, time):
-        randInt = 2
+    def timeToPos(self, time, pathIndex):
+        tempPoint = self.pathes[pathIndex].returnPoint(time)
+        nextPoint = self.pathes[pathIndex].returnPoint(time + 1)
+
+        returnX, returnY = tempPoint.getX(), tempPoint.getY()
+
+        if returnX + 1 == nextPoint.getX() :
+            returnX += time % 1
+        elif returnX - 1 == nextPoint.getX() :
+            returnX -= time % 1
+        elif returnY + 1 == nextPoint.getY() :
+            returnY += time % 1
+        elif returnY - 1 == nextPoint.getY() :
+            returnY -= time % 1
+
+        return self.Point(returnX, returnY)
 
 # What is contained in a single grid point
 class SingleGrid():
-    REQ_ATTRS = ["hasTower", "hasEnemy", "hasPath", "hasEnd"]
+    REQ_ATTRS = ["hasTower", "hasEnemy", "hasPath", "hasEnd", "hasStart"]
     DEFAULT_ATTRIBUTES = {
         "hasTower" : False,
         "hasEnemy" : False,
         "hasPath" : False,
-        "hasEnd" : False
+        "hasEnd" : False,
+        "hasStart" : False
     }
     def __init__(self):
         self.hasTower = False
         self.hasEnemy = False
         self.hasPath = False
         self.hasEnd = False
+        self.hasStart = False
 
     def getPath(self):
         return self.hasPath
@@ -102,6 +171,9 @@ class SingleGrid():
     def getEnd(self):
         return self.hasEnd
 
+    def getStart(self):
+        return self.hasStart
+
     def changeTower(self, newTower):
         self.hasTower = newTower
 
@@ -110,3 +182,19 @@ class SingleGrid():
 
     def changeEnd(self, newEnd):
         self.hasEnd = newEnd
+
+    def changeStart(self, newStart):
+        self.hasStart = newStart
+
+class Path():
+    def __init__(self):
+        self.allPoints = []
+
+    def addPoint(self, newPoint):
+        self.allPoints.append(newPoint)
+
+    def returnPoint(self, index):
+        return self.allPoints[index]
+
+    def returnLength(self):
+        return len(self.allPoints)
