2e4ba99348ddef7c48325d118436b9318b9b5822
Sophia <20sophia.duncan@boiseschools.net>
2020-03-03 13:01:17 -0700

    Initial environment testing

--- game/testing/environment_test.py
+++ game/testing/environment_test.py
@@ -3,29 +3,37 @@ from game.subsystems.environment import *
 from pygame.locals import *
 pygame.init()
 
-def main():
-    test = Environment()
-    testingString = ""
-    for i in range(0, len(test.board[0]), 1):
-        testingString = ""
-        for j in range(0, len(test.board), 1):
-            if test.board[j][i].getPath():
-                testingString += " P"
-            else:
-                testingString += " N"
-        print(testingString)
+class Testing ():
+    def __init__(self):
+        self.test = Environment()
 
-    added = False
-    for i in range(0, len(test.board[0]), 1):
+    def pathTest(self):
         testingString = ""
-        for j in range(0, len(test.board), 1):
-            for k in range(0, test.pathes[0].pathLength() - 1, 0):
-                tempPoint = test.pathes[0].returnPoint(k)
-                if tempPoint.getX() == i and tempPoint.getY() == j:
+        for i in range(0, len(self.test.board[0]), 1):
+            testingString = ""
+            for j in range(0, len(self.test.board), 1):
+                if self.test.board[j][i].getPath():
                     testingString += " P"
-                    added = True
-            if not added:
-                testingString += " N"
+                else:
+                    testingString += " N"
+            print(testingString)
 
-main()
+        added = False
+        for i in range(0, len(self.test.board[0]), 1):
+            testingString = ""
+            for j in range(0, len(self.test.board), 1):
+                for k in range(0, self.test.pathes[0].pathLength() - 1, 0):
+                    tempPoint = self.test.pathes[0].returnPoint(k)
+                    if tempPoint.getX() == i and tempPoint.getY() == j:
+                        testingString += " P"
+                        added = True
+                if not added:
+                    testingString += " N"
+
+        def timeTest(self):
+            randInt = 2
+
+envirTest = Testing()
+
+envirTest.pathTest()
 
