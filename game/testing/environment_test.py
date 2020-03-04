6aa0831f6cda8b0595abd0149d555d7cb1a5b27e
Sophia <20sophia.duncan@boiseschools.net>
2020-03-04 13:01:46 -0700

    Adding visual testing

--- game/testing/environment_test.py
+++ game/testing/environment_test.py
@@ -14,6 +14,10 @@ class Testing ():
             for j in range(0, len(self.test.board), 1):
                 if self.test.board[j][i].getPath():
                     testingString += " P"
+                elif self.test.board[j][i].getEnd():
+                    testingString += " E"
+                elif self.test.board[j][i].getStart():
+                    testingString += " S"
                 else:
                     testingString += " N"
             print(testingString)
@@ -24,16 +28,42 @@ class Testing ():
             for j in range(0, len(self.test.board), 1):
                 for k in range(0, self.test.pathes[0].pathLength() - 1, 0):
                     tempPoint = self.test.pathes[0].returnPoint(k)
+                    tempPoint = self.test.Point(1, 1)
                     if tempPoint.getX() == i and tempPoint.getY() == j:
                         testingString += " P"
                         added = True
                 if not added:
                     testingString += " N"
 
-        def timeTest(self):
-            randInt = 2
+    def timeTest(self):
+        #Colours
+        BROWN = (150, 75, 0)
+        LIGHTBROWN = (181, 101, 29)
+        BLACK = (0, 0, 0)
+        RED = (255, 0, 0)
 
-envirTest = Testing()
+        SCREEN = pygame.display.set_mode((50 * len(self.test.board), 50 * len(self.test.board[0])))
+        pygame.draw.rect(SCREEN, BROWN, (0,0, 50 * len(self.test.board), 50 * len(self.test.board[0])))
 
-envirTest.pathTest()
+        for i in range(0, len(self.test.board[0]), 1):
+            for j in range(0, len(self.test.board), 1):
+                if self.test.board[j][i].getPath():
+                    pygame.draw.rect(SCREEN, LIGHTBROWN, (50 * j, 50 * i, 50, 50))
+                if self.test.board[j][i].getStart():
+                    pygame.draw.rect(SCREEN, LIGHTBROWN, (50 * j, 50 * i, 50, 50))
+                if self.test.board[j][i].getEnd():
+                    pygame.draw.rect(SCREEN, BLACK, (50 * j, 50 * i, 50, 50))
 
+        pygame.display.update()
+
+
+def main():
+    envirTest = Testing()
+    envirTest.timeTest()
+    while True:
+        for event in pygame.event.get():
+            if event.type == QUIT or (event.type == KEYUP and event.key == K_ESCAPE):
+                pygame.quit()
+                sys.exit()
+
+main()
