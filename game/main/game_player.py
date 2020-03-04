3ab090828435d8094587cb41b886b2ee98757139
veggiebob <veggiebobowman@gmail.com>
2020-03-04 12:41:14 -0700

    more basic game player stuff

--- game/main/game_player.py
+++ game/main/game_player.py
@@ -1,11 +1,19 @@
-from game.subsystems. # todo: import GameState
+from game.subsystems.ui.graphical_user_interface import GUI
 
 from game.common.math import Vector
 
 
 class GamePlayer:
-    def __init__ (self, game_state: GameState, gui: GUI):
+    def __init__ (self, game_state: GameState, gui: GUI): # todo: import GameState
         self.game_state = game_state
         self.gui = gui
+        self.holding_tower = False
+        self.held_tower = None # tower that is being dragged around by the mouse
 
-    def update (self, mouse_position: Vector, mouse_delta: float):
+    def update (self, mouse_position: Vector, mouse_down: bool, mouse_pressed: bool):
+        self.gui.update(mouse_position, mouse_down, mouse_pressed)
+
+    def draw_to_surface (self, surface):
+        game_area_panel = self.gui.
+        surface.blit()
+        self.gui.draw_to_surface(surface, True)
