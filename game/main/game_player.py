7376444807ad8abe35d7e641e81e56cc86da395f
veggiebob <veggiebobowman@gmail.com>
2020-03-09 13:00:38 -0600

    game player more stuff

--- game/main/game_player.py
+++ game/main/game_player.py
@@ -8,15 +8,19 @@ class GamePlayer:
         self.game_state = game_state
         self.gui = gui
 
-    def set_on_click_for_element (self, name: str, onclick) -> None:
+    def set_on_click_for_element (self, name: str, onclick) -> None: # to be used in main
         self.gui.get_element(name).set_on_click_listener(onclick)
 
-    def set_on_click_args_for_element (self, name: str, args: dict) -> None:
+    def set_on_click_args_for_element (self, name: str, args: dict) -> None: # to be used in main
         self.gui.get_element(name).set_click_args(args)
+
+    def set_element_click (self, name: str, onclick, args: dict) -> None:
+        self.set_on_click_for_element(name, onclick)
+        self.set_on_click_args_for_element(name, args)
 
     def update (self, mouse_position: Vector, mouse_down: bool, mouse_pressed: bool):
         self.gui.update(mouse_position, mouse_down, mouse_pressed)
-        self.game_state.update(mouse_position, mouse_down, mouse_pressed) # todo: ui shouldn't handle game interaction
+        self.game_state.update(mouse_position.to_tuple(), mouse_down, mouse_pressed) # todo: ui shouldn't handle game interaction
         # mouse_pressed is only true the instant the mouse is actually pressed down
         # mouse_down is true whenever the mouse is pressed down
         # mouse_position is the position, in pixels. Transform by environment . . . ?
@@ -25,6 +29,7 @@ class GamePlayer:
         game_area_panel = self.gui.get_panel('game_area') # todo: make sure this works
         # todo: request a background of this width and height. Changes in size should be handled in art,
         #  and game state should be given a background of this dynamic size, essentially provided by Elijah's code
+        #  or maybe just clip the surface in the game area
         background = self.game_state.get_background(game_area_panel.width, game_area_panel.height)
         surface.blit((int(game_area_panel.x), int(game_area_panel.y)), background)
         for e in self.game_state.get_enemies(): # todo: gets Enemy Surfaces with locations
